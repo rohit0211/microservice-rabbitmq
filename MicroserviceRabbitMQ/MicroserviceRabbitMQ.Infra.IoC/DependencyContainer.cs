d9b92600822190ca6f5a2f84203a75c5ca5356b0
rohit0211 <rohit0268@outlook.com>
2022-09-15 19:51:41 +0530

    initial commit for dependecies

---
+++ MicroserviceRabbitMQ/MicroserviceRabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroserviceRabbitMQ.Domain.Core.Bus;
+using MicroserviceRabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceRabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection servcies)
+        {
+            servcies.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
